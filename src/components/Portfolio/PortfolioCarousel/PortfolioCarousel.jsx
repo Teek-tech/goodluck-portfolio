5c696faa21df45903d6297dab881d9d3458f68eb
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-12-24 03:00:48 +0100

    change carousel link to hash

--- src/components/Portfolio/PortfolioCarousel/PortfolioCarousel.jsx
+++ src/components/Portfolio/PortfolioCarousel/PortfolioCarousel.jsx
@@ -67,7 +67,7 @@ const PortfolioCarousel = () => {
                                         <img src={project.img} alt={project.title} />
                                         {isActive ?
                                             <StyledLink style={display}>
-                                                <Link to="/resources">
+                                                <Link to="#">
                                                     <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                         <path d="M24 36C35.046 36 44 24 44 24C44 24 35.046 12 24 12C12.954 12 4 24 4 24C4 24 12.954 36 24 36Z" stroke="white" strokeWidth="4" strokeLinejoin="round"/>
                                                         <path d="M24 29C25.3261 29 26.5979 28.4732 27.5355 27.5355C28.4732 26.5979 29 25.3261 29 24C29 22.6739 28.4732 21.4021 27.5355 20.4645C26.5979 19.5268 25.3261 19 24 19C22.6739 19 21.4021 19.5268 20.4645 20.4645C19.5268 21.4021 19 22.6739 19 24C19 25.3261 19.5268 26.5979 20.4645 27.5355C21.4021 28.4732 22.6739 29 24 29Z" stroke="white" strokeWidth="4" strokeLinejoin="round"/>
