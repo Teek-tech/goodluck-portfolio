b69dd320d86160682d729772af80f590c5814a32
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2022-02-13 14:41:58 +0100

    fix nav bug

--- src/pages/Portfolio/styles.js
+++ src/pages/Portfolio/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 
 export const StyledWrapper = styled.div`
 
-    @media(max-width: 800px){
+    /* @media(max-width: 800px){
         margin-top: 55%;
-    }
+    } */
 
     @media(min-width: 1200px){
         height: 50vh;
